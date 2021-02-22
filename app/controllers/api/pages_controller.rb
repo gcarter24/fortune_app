e068bccb010fb88d7e828b75ebad6500b2e159b5
George <gchriscarter@gmail.com>
2021-02-22 15:30:03 -0500

    add fortunes array

--- app/controllers/api/pages_controller.rb
+++ app/controllers/api/pages_controller.rb
@@ -1,17 +1,23 @@
 class Api::PagesController < ApplicationController
-  def fortune1
-    render "fortune1.json.jb" #json: { message: "hello" }
+  attr_accessor :random_fortune
+
+  def fortune
+    fortunes = ["Good news is on the horizon", "You will be greeted by three ghosts tonight", "You will be spoiled with riches", "You will create the next Facebook!"]
+
+    @random_fortune = fortunes.sample
+    render "fortune.json.jb" #json: { message: "hello" }
   end
 
   def fortune2
+    @message = "You will be spoiled with riches"
     render "fortune2.json.jb"
   end
 
-  def fortune3
-    render "fortune3.json.jb"
-  end
+  #   def fortune3
+  #     render "fortune3.json.jb"
+  #   end
 
-  def fortune4
-    render "fortune4.json.jb"
-  end
+  #   def fortune4
+  #     render "fortune4.json.jb"
+  #   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   namespace :api do
-    get "/fortune1" => "pages#fortune1"
+    get "/fortune" => "pages#fortune"
     get "/fortune2" => "pages#fortune2"
-    get "/fortune3" => "pages#fortune3"
-    get "/fortune4" => "pages#fortune4"
+    # get "/fortune3" => "pages#fortune3"
+    # get "/fortune4" => "pages#fortune4"
   end
 end
